5046efba49c744552df9e73e335e8d4d405fd97d
Daniil Velichko <daniil@squaredu.cc>
2022-05-09 16:20:00 +0700

    Make context setters non mut

--- src/context.rs
+++ src/context.rs
@@ -13,75 +13,81 @@ impl Context {
         }
     }
 
-    pub fn set_bool<S>(mut self, name: S, value: bool) -> Self
+    pub fn set_bool<S>(self, name: S, value: bool) -> Self
     where
         S: Into<String>,
     {
-        self.data.insert(name.into(), Value::Bool(value));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::Bool(value));
+        Context { data }
     }
 
-    pub fn set_bool_array<S, V>(mut self, name: S, value: V) -> Self
+    pub fn set_bool_array<S, V>(self, name: S, value: V) -> Self
     where
         S: Into<String>,
         V: Into<Vec<bool>>,
     {
-        self.data
-            .insert(name.into(), Value::BoolArray(value.into()));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::BoolArray(value.into()));
+        Context { data }
     }
 
-    pub fn set_int<S>(mut self, name: S, value: i64) -> Self
+    pub fn set_int<S>(self, name: S, value: i64) -> Self
     where
         S: Into<String>,
     {
-        self.data.insert(name.into(), Value::Int(value));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::Int(value));
+        Context { data }
     }
 
-    pub fn set_int_array<S, V>(mut self, name: S, value: V) -> Self
+    pub fn set_int_array<S, V>(self, name: S, value: V) -> Self
     where
         S: Into<String>,
         V: Into<Vec<i64>>,
     {
-        self.data.insert(name.into(), Value::IntArray(value.into()));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::IntArray(value.into()));
+        Context { data }
     }
 
-    pub fn set_float<S>(mut self, name: S, value: f64) -> Self
+    pub fn set_float<S>(self, name: S, value: f64) -> Self
     where
         S: Into<String>,
     {
-        self.data.insert(name.into(), Value::Float(value));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::Float(value));
+        Context { data }
     }
 
-    pub fn set_float_array<S, V>(mut self, name: S, value: V) -> Self
+    pub fn set_float_array<S, V>(self, name: S, value: V) -> Self
     where
         S: Into<String>,
         V: Into<Vec<f64>>,
     {
-        self.data
-            .insert(name.into(), Value::FloatArray(value.into()));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::FloatArray(value.into()));
+        Context { data }
     }
 
-    pub fn set_str<S, V>(mut self, name: S, value: V) -> Self
+    pub fn set_str<S, V>(self, name: S, value: V) -> Self
     where
         S: Into<String>,
         V: Into<String>,
     {
-        self.data.insert(name.into(), Value::Str(value.into()));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::Str(value.into()));
+        Context { data }
     }
 
-    pub fn set_str_array<S, V>(mut self, name: S, value: V) -> Self
+    pub fn set_str_array<S, V>(self, name: S, value: V) -> Self
     where
         S: Into<String>,
         V: Into<Vec<String>>,
     {
-        self.data.insert(name.into(), Value::StrArray(value.into()));
-        self
+        let mut data = self.data;
+        data.insert(name.into(), Value::StrArray(value.into()));
+        Context { data }
     }
 
     pub fn get(&self, name: &str) -> Option<&Value> {
